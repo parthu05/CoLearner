50ae9fe025e783fb1661f5cb73f86bf42160742c
parthu05 <parthusiraboina@gmail.com>
2025-11-08 20:38:49 +0530

    added rooms

--- base/views.py
+++ base/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
 def home(req):
-    return HttpResponse("Hello, World!")
+    return render(req, 'home.html',{'rooms':rooms})
+
+def room(req,pk):
+    room = None
+    for r in rooms:
+        if r["id"] == int(pk):
+            room = r
+            break
+    context = {'room':room}
+    return render(req, 'room.html',context)
