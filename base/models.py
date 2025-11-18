41e39744edc6e3c08cbc31f06a0ce792ad3d0fa4
parthu05 <parthusiraboina@gmail.com>
2025-11-18 22:58:23 +0530

    changes with the rooms in home page

--- base/models.py
+++ base/models.py
@@ -13,7 +13,7 @@ class Room (models.Model):
     topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     participants = models.ManyToManyField(User,related_name='participants',blank=True)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True, max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now = True)
 
